749ec9ce7f6bee437a95af3c181a42137d03e955
PhotonRayleigh <PhotonRayleigh@protonmail.com>
2022-02-19 14:57:07 -0500

    Remove old comments in preparation to start clean with DI and Bloc.

--- lib/program.dart
+++ lib/program.dart
@@ -8,25 +8,8 @@ import 'package:spark_lib/navigation/spark_nav.dart';
 import 'app/screens/dnd_home.dart';
 import 'app/theme/base_theme.dart';
 
-// final Program program = Program();
-
 class Program {
   Program(this.app, this.windowData);
-
-  // GlobalKey<AppSystemManagerState> appManagerKey =
-  //     GlobalKey(debugLabel: "System Manager");
-
-  // AppSystemManagerState get appManager {
-  //   var state = appManagerKey.currentState;
-  //   if (state == null) throw "AppSystemManager has not been initialized.";
-  //   return state;
-  // }
-
-  // late SparkApp app = SparkApp(
-  //     home: home,
-  //     title: "D&D App",
-  //     sysManagerKey: appManagerKey,
-  //     theme: baseTheme);
 
   SparkApp app;
   WindowData windowData;
@@ -48,27 +31,3 @@ class WindowData {
   Alignment windowAlignment = Alignment.center;
   String windowTitle = "D&D Character Editor";
 }
-
-/*
-  Application structure:
-
-  Singletons ➡ Top level ambient services, off-main logic
-  ↕
-  Application Classes
-    Contains:
-      ➡ Main application logic
-      ➡ App data
-      ➡ Widget tree
-        ➡ Controllers
-        ➡ UI Data
-        ➡ UI Actions
-      ➡ Controllers
-      ➡ Initialize service locators
-
-  The key here is it gets me out of the Widget tree for application logic.
-  Flutter naturally pushes users to make everything widgets, which isn't good
-  for all parts of the program. Encapsulating the widget tree in a
-  Flutter-independent object gives me non-Widget space to store things.
-
-  I could explain it better, but that's as good as I've got for now.
-*/
